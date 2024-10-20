845e43e501c9b09eba778cc49649896462e40c8b
Rômulo <romulonilsonhorn@gmail.com>
2024-10-20 14:27:08 -0300

    base do site

---
+++ pi/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+function Footer() {  
+
+  return (
+    <>
+        <div className='footerContainer'>
+            <img src='./../../public/logoIFC.png' className='logo'/>
+        </div>
+    </>
+  )
+}
+
+export default Footer
---
+++ pi/src/pages/Tecnologias.jsx
@@ -0,0 +1,14 @@
+import './Tecnologias.css'
+
+function Tecnologias() {
+  return (
+    <>
+    <div>
+        <h1>Conteúdo da página de tecnologias</h1>
+    </div> 
+      
+    </>
+  )
+}
+
+export default Tecnologias
