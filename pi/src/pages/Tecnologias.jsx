09b1cbcbb5a107ff0e4fd5de4f41dc7a3a42ea75
aaschenatto <aaschenatto@gmail.com>
2024-10-21 22:23:31 -0300

    primeiras adições na página tecnologias

--- pi/src/pages/Tecnologias.jsx
+++ pi/src/pages/Tecnologias.jsx
@@ -3,9 +3,18 @@ import './Tecnologias.css'
 function Tecnologias() {
   return (
     <>
-    <div>
-        <h1>Conteúdo da página de tecnologias</h1>
-    </div> 
+      <div className='banner'>
+        <img src='../../public/bannerInicio.jpg' />
+      </div>
+
+      <p className='titulo1'>Linha Do Tempo De Tecnologias</p>
+
+      <div className='div1'>
+        <img className='img1' src='../../public/bannerInicio.jpg' />
+        <p className='minititulo'>Tecnologia X</p>
+        <p className='txt1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum.</p>
+      </div>
+
       
     </>
   )
