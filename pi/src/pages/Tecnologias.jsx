2ab432d028223eebda5a884ae794505f96aceef5
aaschenatto <aaschenatto@gmail.com>
2024-10-23 22:16:48 -0300

    pag tecnologias: adicionei div na pagina de tecnologias que podem ser usadas n vezes, adicionei um carrossel que ainda tem que ser ajustado certinho =)

--- pi/src/pages/Tecnologias.jsx
+++ pi/src/pages/Tecnologias.jsx
@@ -8,31 +8,45 @@ function Tecnologias() {
         <img src='../../public/bannerInicio.jpg' />
       </div>
 
+      <div class="container-slider">
+        <button id="prev-button"><img src='../../public/bannerInicio.jpg' alt="prev-button"/></button>
+        <div class="container-images">
+          <img src='../../public/bannerInicio.jpg' alt="girl" class="slider on"/>
+          <img src='../../public/logoIFC.png' alt="girl" class="slider"/>
+        </div>
+        <button id="next-button"><img src='../../public/bannerInicio.jpg' alt="next-button"/></button>
+        </div>
+
+      <div className='divlinha'>
+        <img className='linha' src='../../public/bannerInicio.jpg' />
+      </div>
+
+
       <p className='titulo1'>Linha Do Tempo De Tecnologias</p>
 
       <div className='div1'>
         <img className='img1' src='../../public/bannerInicio.jpg' />
-        <p className='minititulo'>Tecnologia X</p>
+        <p className='minititulo1'>Tecnologia X</p>
         <p className='txt1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum.</p>
       </div>
 
-      <p className='titulo1'>VÍDEOS RELACIONADOS AS TECNOLOGIAS</p>
+      <p className='titulo1'>Vídeos Relacionados As Tecnologias</p>
 
-      <div className='div1'>
-        <img className='img1' src='../../public/bannerInicio.jpg' />
-        <p className='minititulo'>Titulo U</p>
+      <div className='div2'>
+        <img className='img2' src='../../public/bannerInicio.jpg' />
+        <p className='minititulo2'>Titulo U</p>
         <p className='txt2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum.</p>
       </div>
 
-      <div className='div1'>
-        <p className='minititulo'>Titulo W</p>
-        <p className='txt1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum.</p>
-        <img className='img1' src='../../public/bannerInicio.jpg' />
+      <div className='div3'>
+        <p className='minititulo3'>Titulo W</p>
+        <p className='txt3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum.</p>
+        <img className='img3' src='../../public/bannerInicio.jpg' />
       </div>
 
-      <div className='div1'>
-        <img className='img1' src='../../public/bannerInicio.jpg' />
-        <p className='minititulo'>Titulo P</p>
+      <div className='div2'>
+        <img className='img2' src='../../public/bannerInicio.jpg' />
+        <p className='minititulo2'>Titulo P</p>
         <p className='txt2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum.</p>
       </div>
 
