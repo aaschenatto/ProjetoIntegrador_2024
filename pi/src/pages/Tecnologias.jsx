e2558e90fff46bef16c5a2a926559db93363b026
aaschenatto <126356437+aaschenatto@users.noreply.github.com>
2024-10-27 13:37:28 -0300

    Add files via upload

--- pi/src/pages/Tecnologias.jsx
+++ pi/src/pages/Tecnologias.jsx
@@ -1,21 +1,24 @@
 import './Tecnologias.css'
 
+
 function Tecnologias() {
   return (
     <>
-    
+
+     
+
       <div className='banner'>
         <img src='../../public/bannerInicio.jpg' />
       </div>
 
       <div class="container-slider">
-        <button id="prev-button"><img src='../../public/bannerInicio.jpg' alt="prev-button"/></button>
+        <button id="prev-button"><img src='../../public/arrow.png' alt="prev-button" /></button>
         <div class="container-images">
-          <img src='../../public/bannerInicio.jpg' alt="girl" class="slider on"/>
-          <img src='../../public/logoIFC.png' alt="girl" class="slider"/>
+          <img src='../../public/bannerInicio.jpg' alt="img2" class="slider on" />
+          <img src='../../public/bannerInicio.jpg' alt="img1" class="slider" />
         </div>
-        <button id="next-button"><img src='../../public/bannerInicio.jpg' alt="next-button"/></button>
-        </div>
+        <button id="next-button"><img src='../../public/arrow.png' alt="next-button" /></button>
+      </div>
 
       <div className='divlinha'>
         <img className='linha' src='../../public/bannerInicio.jpg' />
@@ -49,6 +52,7 @@ function Tecnologias() {
         <p className='minititulo2'>Titulo P</p>
         <p className='txt2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum.</p>
       </div>
+
 
     </>
   )
