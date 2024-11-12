a14eb19a3a9d6c698683eab90ec505a506c27bc6
Rômulo <139631832+Romulooo@users.noreply.github.com>
2024-11-12 14:14:36 -0300

    Update Tecnologias.jsx

--- pi/src/pages/Tecnologias.jsx
+++ pi/src/pages/Tecnologias.jsx
@@ -1,22 +1,20 @@
 import './Tecnologias.css'
-
+import '../../script.js'
 
 function Tecnologias() {
+  
   return (
     <>
-
-     
-
       <div className='banner'>
         <h1>TECNOLOGIAS</h1>
-        <img src='../../public/bannerInicio.jpg' />
+        <img src='../../public/bannerTecnologias.jpg' />
       </div>
 
-      <div class="container-slider">
+      <div className="container-slider">
         <button id="prev-button"><img src='../../public/arrow.png' alt="prev-button" /></button>
-        <div class="container-images">
-          <img src='../../public/bannerInicio.jpg' alt="img2" class="slider on" />
-          <img src='../../public/bannerInicio.jpg' alt="img1" class="slider" />
+        <div className="container-images">
+          <img src='../../public/bannerInicio.jpg' alt="img2" className="slider on" />
+          <img src='../../public/bannerInicio.jpg' alt="img1" className="slider" />
         </div>
         <button id="next-button"><img src='../../public/arrow.png' alt="next-button" /></button>
       </div>
@@ -28,27 +26,25 @@ function Tecnologias() {
 
       <p className='titulo1'>Linha Do Tempo De Tecnologias</p>
 
-      <div className='div3'>
-        <p className='minititulo3'>Titulo W</p>
-        <p className='txt3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum.</p>
-        <img className='img3' src='../../public/bannerInicio.jpg' />
+      <div className='divEsquerda'>
+        <h2>Titulo W</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum.</p>
+        <img src='../../public/bannerInicio.jpg' />
       </div>
 
       <p className='titulo1'>Vídeos Relacionados As Tecnologias</p>
 
-      <div className='div2'>
-        <img className='teste' src='../../public/bannerInicio.jpg'/>
-        <p className='mini'>Titulo F</p>
-        
-        <p className='teste1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum.</p>
+      <div className='divDireita'>
+        <img src='../../public/bannerInicio.jpg'/>
+        <h2>Titulo F</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum.</p>
       </div>
 
-      <div className='div3'>
-        <p className='minititulo3'>Titulo W</p>
-        <p className='txt3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum.</p>
-        <img className='img3' src='../../public/bannerInicio.jpg' />
+      <div className='divEsquerda'>
+        <h2>Titulo W</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum.</p>
+        <img src='../../public/bannerInicio.jpg' />
       </div>
-
    
 
     </>
@@ -56,4 +52,3 @@ function Tecnologias() {
 }
 
 export default Tecnologias
-
