20ba4bf29d7aae333ab643862f3ab1ed2251d41a
Rômulo <139631832+Romulooo@users.noreply.github.com>
2024-11-12 14:13:51 -0300

    Add files via upload

---
+++ pi/src/pages/Sobre.jsx
@@ -0,0 +1,14 @@
+import './Sobre.css'
+
+function Sobre() {
+  return (
+    <>
+      <div className='banner'>
+          <h1>SOBRE NÓS</h1>
+          <img src='../../public/bannerInicio.jpg'/>
+      </div>
+    </>
+  )
+}
+
+export default Sobre
