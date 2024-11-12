7731b9f87fecd23c5644c4f5d88d2e0b3402b00c
Rômulo <139631832+Romulooo@users.noreply.github.com>
2024-11-12 14:15:38 -0300

    Update script.js

--- pi/script.js
+++ pi/script.js
@@ -1,5 +1,6 @@
 //cod carrosel//
 
+/*
 const slider = document.querySelectorAll('.slider');
 const btnPrev = document.getElementById('prev-button');
 const btnNext = document.getElementById('next-button');
@@ -36,9 +37,43 @@ function prevSlider() {
 
 btnNext.addEventListener('click', nextSlider)
 btnPrev.addEventListener('click', prevSlider)
+*/
 
-// fim do cod carrosel//
 
-function teste() {
-    console.log("esta certo!")
-}
+document.addEventListener("DOMContentLoaded", function(){
+  setTimeout(function() {
+  console.log("DOM fully loaded and parsed");
+  let slider = document.querySelectorAll('.slider');
+  let currentSlide = 0;
+  function hideSlider() {
+    slider.forEach(item => item.classList.remove('on'))
+  }
+  function showSlider() {
+    slider[currentSlide].classList.add('on')
+  }
+  function nextSlider() {
+    hideSlider()
+    if(currentSlide === slider.length -1) {
+      currentSlide = 0
+    } else {
+      currentSlide++
+    }
+    showSlider()
+  }
+
+  function prevSlider() {
+    hideSlider()
+    if(currentSlide === 0) {
+      currentSlide = slider.length -1
+    } else {
+      currentSlide--
+    }
+    showSlider()
+  }
+  document.getElementById('next-button').addEventListener('click', nextSlider);
+  document.getElementById('prev-button').addEventListener('click', prevSlider);
+  console.log('aa');
+}, 3000);
+});
+  
+  
