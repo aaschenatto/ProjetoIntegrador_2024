874f7ba6b77c1950886f13769f60c032d7bdd7b5
Rômulo <139631832+Romulooo@users.noreply.github.com>
2024-11-27 19:49:24 -0300

    Add files via upload

---
+++ pi/src/pages/Desafios.jsx
@@ -0,0 +1,15 @@
+import './Desafios.css'
+
+function Desafios() {
+  return (
+    <>
+      <div className='banner'>
+          <h1>DESAFIOS</h1>
+          <img src='../../public/bannerInicio.jpg'/>
+      </div>
+
+    </>
+  )
+}
+
+export default Desafios
