0f5a685eae22606914c610318fbb6cb2207c4b78
Rômulo <139631832+Romulooo@users.noreply.github.com>
2024-12-10 13:55:22 -0300

    Update script.js

--- pi/script.js
+++ pi/script.js
@@ -1,45 +1,15 @@
-//cod carrosel//
+window.onscroll = function () {
+  scroll();
+};
+
+function scroll() {
+  document.getElementById("marte").style.transform = "rotate(" + window.pageYOffset/32 + "deg)";
+  document.getElementById("asteroide").style.transform = "rotate(" + window.pageYOffset/-64 + "deg)";
+  document.getElementById("lua").style.transform = "rotate(" + window.pageYOffset/32 + "deg)";
+  document.getElementById("nave").style.transform = "rotate(" + window.pageYOffset/-64 + "deg)";
+}
 
 /*
-const slider = document.querySelectorAll('.slider');
-const btnPrev = document.getElementById('prev-button');
-const btnNext = document.getElementById('next-button');
-
-let currentSlide = 0;
-
-function hideSlider() {
-  slider.forEach(item => item.classList.remove('on'))
-}
-
-function showSlider() {
-  slider[currentSlide].classList.add('on')
-}
-
-function nextSlider() {
-  hideSlider()
-  if(currentSlide === slider.length -1) {
-    currentSlide = 0
-  } else {
-    currentSlide++
-  }
-  showSlider()
-}
-
-function prevSlider() {
-  hideSlider()
-  if(currentSlide === 0) {
-    currentSlide = slider.length -1
-  } else {
-    currentSlide--
-  }
-  showSlider()
-}
-
-btnNext.addEventListener('click', nextSlider)
-btnPrev.addEventListener('click', prevSlider)
-*/
-
-
 document.addEventListener("DOMContentLoaded", function(){
   setTimeout(function() {
   console.log("DOM fully loaded and parsed");
@@ -76,4 +46,4 @@ document.addEventListener("DOMContentLoaded", function(){
 }, 3000);
 });
   
-  
+*/
