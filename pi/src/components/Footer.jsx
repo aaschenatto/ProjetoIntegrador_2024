7ec80fdc631a990863e46c9db9df474ca432e5f3
aaschenatto <126356437+aaschenatto@users.noreply.github.com>
2024-12-21 17:22:11 -0300

    Add files via upload

--- pi/src/components/Footer.jsx
+++ pi/src/components/Footer.jsx
@@ -5,27 +5,27 @@ function Footer() {
   return (
     <>
 
-      <div className='footerContainer'>
-        <img src='./../../public/logoIFC.png' className='logo' />
+<div className='footerContainer'>
+  <img src='./../../public/logoIFC.png' className='logo' />
 
-        <div className='newsletter'>
-          <p className='titulonews'>Quer receber notícias relacionadas ao nosso conteúdo?</p>
-          <p className='titulonews'>Preencha o campo abaixo com seu e-mail:</p>
-          <input placeholder='E-mail...'></input>
-          <button>Enviar</button>
-        </div>
-      <div className='redes0'>
-        <div className='redes1'>
-          <p  className='nome'>@nossoInsta</p>
-          <img className='logoRede' src='../../public/insta.png'/>
-        </div>
-        <div className='redes'>
-          <p className='nome'>email@gmail.com</p>
-          <img className='logoRede' src='../../public/email.png'/>
-        </div>
-      </div>
+  <div className='newsletter'>
+    <p className='titulonews'>Quer receber notícias relacionadas ao nosso conteúdo?</p>
+    <p className='titulonews'>Preencha o campo abaixo com seu e-mail:</p>
+    <input placeholder='E-mail...'></input>
+    <button>Enviar</button>
+  </div>
 
-      </div>
+  <div className='redes'>
+    <div className='insta'>
+      <a href="https://www.instagram.com/mineracao.espacial/" className='nome'><p className='nome'>@mineracao.espacial</p></a>
+      <img className='logoRede' src='../../public/insta.png'/>
+    </div>
+    <div className='email'>
+      <p className='nome'> mineracaoespacial@gmail.com</p>
+      <img className='logoRede' src='../../public/email.png'/>
+    </div>
+    </div>
+</div>
 
     </>
   )
