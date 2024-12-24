e03a8c34e451ab265987cdf0385a2bc4362bcd8f
Rômulo <139631832+Romulooo@users.noreply.github.com>
2024-12-24 12:00:53 -0300

    Arquivo infoElemento

---
+++ pi/src/components/InfoElemento.jsx
@@ -0,0 +1,15 @@
+function InfoElemento(props) {
+    
+    return (
+        <>
+            <section className={props.classe}>
+                <span>{props.n_atomico}</span>
+                <h2>{props.simbolo}</h2>
+                <p>{props.nome}</p>
+                <p>{props.peso}</p>
+            </section>
+        </> 
+    )
+}
+
+export default InfoElemento
