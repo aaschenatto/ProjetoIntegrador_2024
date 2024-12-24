4ec80352d7dd1867546a790f502c596f1e79fe23
aaschenatto <126356437+aaschenatto@users.noreply.github.com>
2024-12-24 18:09:06 -0300

    Add files via upload

--- pi/src/Site.jsx
+++ pi/src/Site.jsx
@@ -7,6 +7,7 @@ import Recursos from "./pages/Recursos.jsx"
 import Sobre from "./pages/Sobre.jsx";
 import Desafios from "./pages/Desafios.jsx";
 import Footer from "./components/Footer.jsx";
+import Contato from "./pages/Contato.jsx";
 
 export default function Site() {
   const [currentPage, setCurrentPage] = useState(1);
@@ -19,7 +20,8 @@ export default function Site() {
       {currentPage === 3 && <Recursos />}
       {currentPage === 4 && <Desafios />}
       {currentPage === 5 && <Sobre />}
-      <Footer/>
+      {currentPage === 6 && <Contato />}
+      <Footer setCurrentPage={setCurrentPage}/>
     </>
   );
 }
