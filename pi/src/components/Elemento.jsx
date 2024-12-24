21d3dcdde83b0d856260ba89af7c2c619be06516
Rômulo <139631832+Romulooo@users.noreply.github.com>
2024-12-24 12:10:46 -0300

    Update Elemento.jsx

--- pi/src/components/Elemento.jsx
+++ pi/src/components/Elemento.jsx
@@ -1,7 +1,8 @@
 function Elemento(props) {
+    
     return (
         <>
-            <section className="elemento">
+            <section className={props.classe}>
                 <span>{props.n_atomico}</span>
                 <h2>{props.simbolo}</h2>
                 <p>{props.nome}</p>
