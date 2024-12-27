03cbfed07b132920e5adc8b8a4467676522f43dc
aaschenatto <126356437+aaschenatto@users.noreply.github.com>
2024-12-27 14:13:23 -0300

    Add files via upload

--- pi/src/pages/Desafios.jsx
+++ pi/src/pages/Desafios.jsx
@@ -8,7 +8,11 @@ function Desafios() {
           <img src='../../public/bannerDesafios.jpg'/>
       </div>
 
-      <p>qaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+<div className='int'>
+      <h2 className='tituloInt'>Desafios e suas soluções</h2>
+
+      <p className='txtInt'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum. consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</p>
+</div>
 
     </>
   )
